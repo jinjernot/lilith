a460572321ffb198f64e34ecc05aeca5df4411e8
jinjernot <norindes@gmail.com>
2024-08-12 13:02:48 -0600

    main mamado

--- app/core/main.py
+++ app/core/main.py
@@ -1,10 +1,9 @@
 #from app.core.xml_parser_lite import process_data
-from xml_parser import process_data
-from models import model
-from config import XML_PATH, CLASS_LABELS
+from xml_parser_lite import process_data
+from config import XML_PATH
 
 def main():
-    process_data(XML_PATH, model, CLASS_LABELS)
+    process_data(XML_PATH)
 
 if __name__ == '__main__':
     main()
